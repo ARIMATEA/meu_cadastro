65fa4339e9b319c81e31710884333b6ca021679f
ARIMATEA <arilafayette@gmail.com>
2016-06-04 12:10:51 -0300

    Testes de formunarios

--- hello.php
+++ hello.php
@@ -5,5 +5,13 @@
 <body>
 	<h1>Seja bem-vindo</h1>
 	<h2>Informe seus dados</h2>
+	<form method='POST' action=''>
+		<label>Nome:</label>
+		<input name='nome_usuario' type = 'text'>
+		<label>Email</label>
+		<input name='email_usuario' type='email'>
+		<input name='salvar' value ="Confirmar" type ='submit'>
+		<input name='salvar' value ="Limpar" type ='reset'>
+	</form>
 </body>
 </html>
